58067b6a7cbe6e724f00bb7ef21d9362a7de49fd
Necu16 <er.neculan@duocuc.cl>
2024-10-13 04:21:26 -0300

    implementacion en android

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  // Animaciones de Angular
+import { MatCardModule } from '@angular/material/card';  // Componente de Angular Material
 
-// Importa el módulo de Ionic Storage
+// Importa el módulo de Ionic Storage (si es necesario)
 import { IonicStorageModule } from '@ionic/storage-angular';
 // Importa SQLite
 import { SQLite } from '@ionic-native/sqlite/ngx';
+// Importa HttpClientModule
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,14 +21,14 @@ import { SQLite } from '@ionic-native/sqlite/ngx';
     IonicModule.forRoot(), 
     AppRoutingModule,
     MatCardModule,
-    // Configura Ionic Storage en la aplicación
-    IonicStorageModule.forRoot()
+    HttpClientModule,  // Módulo para realizar peticiones HTTP
+    BrowserAnimationsModule,  // Módulo de animaciones de Angular
+    IonicStorageModule.forRoot()  // Solo si planeas usar Ionic Storage
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideAnimationsAsync(),
-    SQLite  // Registra SQLite como proveedor
+    SQLite  // Proveedor de SQLite
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
